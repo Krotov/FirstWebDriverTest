66cf48fd87a680deab3d04d5cc6c677ce86b9f47
Anatolii Krotov <anatolii.krotov@gmail.com>
2015-10-09 14:42:48 +0300

    Add First Selenium Test

---
+++ src/test/java/ua/pp/krotov/FirstTest.java
@@ -0,0 +1,55 @@
+package ua.pp.krotov;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class FirstTest {
+	
+	private WebDriver driver;
+	
+	@BeforeClass
+	public void setUp() {
+		// Create a new instance of the Firefox driver
+		driver = new FirefoxDriver();
+	}
+	
+	@Test
+	public void gotoSeleniumWikiPage() {
+		// Go to the Wikipedia home page
+		driver.get("https://en.wikipedia.org/");
+		// Find the text input element by its id and type "Selenium"
+		driver.findElement(By.id("searchInput")).sendKeys("Selenium");
+		// Click search button		
+		driver.findElement(By.id("searchButton")).click();
+		// Get text from header of the Selenium page		
+		String header = driver.findElement(By.id("firstHeading")).getText();
+		// Verify that header equals "Selenium"		
+		Assert.assertEquals(header, "Selenium");
+	}
+	
+	@Test
+	public void gotoSeleniumWikiPageFailure() {
+		// Go to the Wikipedia home page
+		driver.get("https://en.wikipedia.org/");
+		// Find the text input element by its id and type "Selenium"
+		driver.findElement(By.id("searchInput")).sendKeys("Selenium");
+		// Click search button
+		driver.findElement(By.id("searchButton")).click();
+		// Get text from header of the Selenium page
+		String header = driver.findElement(By.id("firstHeading")).getText();
+		// Verify that header equals "Selenium WebDriver"
+		Assert.assertEquals(header, "Selenium WebDriver");
+	}
+	
+	@AfterClass
+	public void tearDown() {
+		// Closes all browser windows and safely ends the session
+		driver.quit();
+	}
+
+}
